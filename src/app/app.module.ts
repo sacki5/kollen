91349309b8b77e9e39a6d1ac972b074bd93212ca
Isac Ljung <isac.ljung@gmail.com>
2018-04-26 14:21:52 +0200

    Added login, main, topbar, sidenav components and started with templating and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { MainComponent } from './main/main.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    TopbarComponent,
+    SidenavComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
