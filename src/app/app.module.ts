be8b2a9991ba704a668c97f35b650a730c0ff88b
Isac Ljung <isac.ljung@gmail.com>
2018-05-25 11:36:26 +0200

    changed rxjs/rx to rxjs/Rx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Observable } from 'rxjs/rx';
+import { Observable } from 'rxjs/Rx';
 import { MatSidenavModule } from '@angular/material';
 
 import { TableModule } from 'primeng/table';
